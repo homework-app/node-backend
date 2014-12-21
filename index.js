25f62e45461864a29f33a40a86ae120548f0c4f4
Alexis King <lexi.lambda@gmail.com>
2014-12-21 12:42:20 -0800

    Update to use json

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello, world!');
+    res.header('Content-Type', 'application/json');
+    res.send(JSON.stringify({ hello: 'world!' }));
 });
 
 var server = app.listen(process.env.PORT, function () {
