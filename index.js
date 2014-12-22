b0fcdeafd50b90f7819a67eb44e53c55cc17e203
Alexis King <lexi.lambda@gmail.com>
2014-12-21 19:59:12 -0800

    Update sample data

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.get('/', function (req, res) {
     res.header('Content-Type', 'application/json');
-    res.send(JSON.stringify({ hello: 'world!' }));
+    res.send(JSON.stringify({ hello: 'world!', data: [ 1, 2, 3 ] }));
 });
 
 var server = app.listen(process.env.PORT, function () {
